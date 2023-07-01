29434ea917fc324062405b9ae2aacb7484a50f8b
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-07-01 22:43:53 +0300

    update
    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 let buttonScore1 = document.getElementById ('button_score')
-let buttonScore2 = document.getElementById ('button_score')
-let buttonScore3 = document.getElementById ('button_score')
-let buttonScore4 = document.getElementById ('button_score')
-let buttonScore5 = document.getElementById ('button_score')
-let buttonScore6 = document.getElementById ('button_score')
+// let buttonScore2 = document.getElementById ('button_score')
+// let buttonScore3 = document.getElementById ('button_score')
+// let buttonScore4 = document.getElementById ('button_score')
+// let buttonScore5 = document.getElementById ('button_score')
+// let buttonScore6 = document.getElementById ('button_score')
 let resetbtn = document.getElementById ('newGame')
 let homeScore = document.getElementById ('home-score')
 let guestScore = document.getElementById ('guest-score')
@@ -11,6 +11,8 @@ let statsScore = document.getElementById ('stats-score')
 let countHome = 0
 let countGuest = 0
 
+
+//the functions increment_1/2/3 are for home-score board
 
 function increment_1(){
     countHome += 1
@@ -27,6 +29,7 @@ function increment_3(){
     homeScore.textContent = countHome
 
 }
+//the functions icrement_4/5/6 are for guest-score boards
 function increment_4(){
     countGuest += 1
     guestScore.textContent = countGuest
@@ -47,5 +50,13 @@ function reset() {
     countGuest = 0
     homeScore.textContent = countHome
     guestScore.textContent = countGuest
+}
+if (homeScore > guestScore) {
+    statsScore.textC.ontent = 'HOME TEAM GET THE LEAD'
+}else if (homeScore < guestScore){
+    statsScore.textContent = 'GUEST TEAM GET THE LEAD'
+
+}else {
+    statsScore.textContent = 'SCORE IS TIE'
 }
 
